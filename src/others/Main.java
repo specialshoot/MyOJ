be6aa1d26d0ec2535201c88688410118dfcdda53
specialshoot <hanzhi911228@163.com>
2016-09-04 22:16:27 +0800

    20160904

--- src/others/Main.java
+++ src/others/Main.java
@@ -3,47 +3,28 @@ package others;
 import java.util.Scanner;
 
 public class Main {
-
-	public static void getRes(String[] strs) {
-		int x = 0, y = 0;
-		int len = strs.length;
-		if(len==0){
-			System.out.println(x + "," + y);
-			return;
-		}
-		for (int i = 0; i < len; i++) {
-			int length = strs[i].length();
-			if (length <= 1) {
-				continue;
-			}
-			try {
-				String s = strs[i].substring(0, 1);
-				int num = Integer.parseInt(strs[i].substring(1));
-				if (s.equals("A")) {
-					x -= num;
-				} else if (s.equals("D")) {
-					x += num;
-				} else if (s.equals("W")) {
-					y += num;
-				} else if (s.equals("S")) {
-					y -= num;
-				} else {
-					continue;
-				}
-			} catch (Exception e) {
-				continue;
+	
+	public static int getRes(int n){
+		int price=1;
+		int i=3,j=3;
+		for(int m=2;m<=n;++m){
+			if(m==i){
+				i=i+j;
+				j++;
+				--price;
+			}else{
+				++price;
 			}
 		}
-		System.out.println(x + "," + y);
+		return price;
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
 		while (scanner.hasNext()) {
-			String str = scanner.nextLine();
-			String[] strs = str.split(";");
-			getRes(strs);
+			int n=scanner.nextInt();
+			System.out.println(getRes(n));
 		}
 		scanner.close();
 	}
